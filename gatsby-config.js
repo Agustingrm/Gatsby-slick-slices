4bb04510316e75c34fd06d6368801d976bd73b08
Agustin Gramajo <agustingramajo@gmail.com>
2022-01-20 11:43:53 -0300

    Adapted esm

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,2 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env' });
-
-export default {
-  pathPrefix: '/pizza',
-  siteMetadata: {
-    title: 'Slicks Slices',
-    siteUrl: 'https://gatsby.pizza',
-    description: 'The best pizza place',
-    twitter: '@slickSlices',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    {
-      // This is the name of the plugin we are adding
-      resolve: 'gatsby-source-sanity',
-      options: {
-        projectId: 'mlt9qvfj',
-        dataset: 'production',
-        watchMode: true,
-        token: process.env.SANITY_TOKEN,
-      },
-    },
-  ],
-};
+require = require('esm')(module);
+module.exports = require('./gatsby-config.esm.js');
